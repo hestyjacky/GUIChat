a0d3a1e361be4e3beb6083e75e40266e95bdd795
Jackelyn FDZ <92596662+JackyFC534@users.noreply.github.com>
2023-11-29 12:26:17 -0800

    Modificacion de interfaces

--- GUI/src/main/java/com/example/gui/MenuInicialApp.java
+++ GUI/src/main/java/com/example/gui/MenuInicialApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,16 +12,14 @@ public class MenuInicialApp extends Application {
     @Override
     public void start(Stage stage) throws IOException { // main
         FXMLLoader fxmlLoader = new FXMLLoader(InicioApp.class.getResource("MenuInicial.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 640);
+        Scene scene = new Scene(fxmlLoader.load(), 668, 408);
 
-        stage.setMinWidth(560);
-        stage.setMinHeight(680);
+        stage.initStyle(StageStyle.TRANSPARENT); //quita
 
-        stage.setMaxWidth(550);
-        stage.setMaxHeight(670);
-
-
+        /*
         stage.setTitle("Chats");
+
+         */
         stage.setScene(scene);
         stage.onCloseRequestProperty().setValue(event -> System.out.println("\nEnd - Chats!"));
         stage.show();
