6c5f3bd4fe520ebfec0c21c53f2265224daedd37
JackyFC534 <2130173@upv.edu.mx>
2023-12-11 17:35:57 -0600

    mezcla de cosas

--- GUI/src/main/java/com/example/gui/Server.java
+++ GUI/src/main/java/com/example/gui/Server.java
@@ -7,22 +7,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 public class Server{
     private ServerSocket serverSocket;
+    private BufferedReader in;
+    private PrintWriter out;
     public Server(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
 
     public void startServer() {
         try {
-
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
-                System.out.println(socket.getOutputStream());
-                System.out.println("accion dentro de conexion");
-                //System.out.println("New client has connected");
-                ClientHandler clientHandler = new ClientHandler(socket);
+                System.out.println("Nueva conexión entrante");
 
-                Thread thread = new Thread(clientHandler);
-                thread.start();
+                // Lee la consulta enviada por el cliente
+                try (ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream())) {
+                    Object receivedObject = inStream.readObject();
+                    String query = receivedObject.toString();
+
+                    System.out.println("Consulta recibida: " + query);
+                    // Procesa la consulta y envía la respuesta al cliente
+                    String result = SendResultsQuery(query);
+                    // Envia la respuesta al cliente
+                    try (ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream())) {
+                        outStream.writeObject(result);
+                        System.out.println("Respuesta enviada al cliente: " + result);
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                // Cierra el socket del cliente después de manejar la consulta
+                socket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
